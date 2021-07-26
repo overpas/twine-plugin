51636d533761a8859c27b656a76e4d5bcdd03465
overpas <pckeycalculator@gmail.com>
2021-07-27 01:44:36 +0300

    Add comments to Column tests

--- src/test/kotlin/by/overpass/twap/action/translations/TranslationColumnTest.kt
+++ src/test/kotlin/by/overpass/twap/action/translations/TranslationColumnTest.kt
@@ -3,11 +3,17 @@ package by.overpass.twap.action.translations
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
+/**
+ * [TranslationColumn] test
+ */
 class TranslationColumnTest {
 
     private val translationColumn = TranslationColumn()
     private val translationModel = TranslationModel("locale", "text")
 
+    /**
+     * Test that [translationModel] is updated with new text
+     */
     @Test
     fun testValueUpdated() {
         translationColumn.setValue(translationModel, "new text")
@@ -15,6 +21,9 @@ class TranslationColumnTest {
         assertEquals("new text", translationModel.text)
     }
 
+    /**
+     * Test that text is extracted from [translationModel]
+     */
     @Test
     fun testTextFetched() {
         assertEquals("text", translationColumn.valueOf(translationModel))
--- src/test/kotlin/by/overpass/twap/action/translations/TranslationEditingColumnTest.kt
+++ src/test/kotlin/by/overpass/twap/action/translations/TranslationEditingColumnTest.kt
@@ -3,6 +3,9 @@ package by.overpass.twap.action.translations
 import org.junit.Assert.assertTrue
 import org.junit.Test
 
+/**
+ * [TranslationEditingColumn] test
+ */
 @Suppress("INLINE_CLASS_CAN_BE_USED")
 class TranslationEditingColumnTest {
 
@@ -10,6 +13,9 @@ class TranslationEditingColumnTest {
         override fun valueOf(item: TranslationModel): String = ""
     }
 
+    /**
+     * Test that the cell is editable
+     */
     @Test
     fun testIsEditable() {
         assertTrue(translationEditingColumn.isCellEditable(TranslationModel("en", "english")))
